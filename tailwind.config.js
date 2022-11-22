c96291f03bb510e7e35f70c235f8531d6df2a106
Whitnu12 <whitnu@gmail.com>
2022-11-22 09:47:07 +0700

    new hero model

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,6 +5,21 @@ module.exports = {
     extend: {
       colors: {
         primary: { 50: '#eff6ff', 100: '#dbeafe', 200: '#bfdbfe', 300: '#93c5fd', 400: '#60a5fa', 500: '#3b82f6', 600: '#2563eb', 700: '#1d4ed8', 800: '#1e40af', 900: '#1e3a8a' },
+        'coba-blue': '#14213D',
+        'coba-yellow': '#FCA311',
+        'coba-gray': '#E5E5E5',
+      },
+    },
+    fontFamily: {
+      Poppins: ['Poppins,sans-serif'],
+    },
+    container: {
+      center: true,
+      padding: '1rem',
+      screens: {
+        lg: '1124px',
+        xl: '1124px',
+        '2xl': '1124px',
       },
     },
   },
